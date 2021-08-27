b5b3ad69dde33d74ed67494a34e61780aab7a31f
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-08-27 17:02:39 -0400

    Add files via upload

---
+++ e1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("\n************************************************************************\n");
+    printf("\nComentários    na linguagem C iniciam       com /* e termina      com */\n");
+    printf("Letras minúsculas    e maiúsculas       são diferentes na      linguagem C\n");
+    printf("A palavra-chave main       indica o início do programa      na linguagem C\n");
+    printf("Os símbolos {} colchetes envolvem um bloco de      comandos na linguagem C\n");
+    printf("Todas as instruções na linguagem C devem terminar com um ; ponto-e-vírgula\n");
+    printf("\n************************************************************************\n");
+    return 0;
+}
---
+++ e2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf("Alô! ");
+printf("Alô! ");
+printf("Tem alguém aí?");
+printf("\n");
+return 0;
+}
