b0b383440e5558b65fd587cf69498ac27a9a295d
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-09-30 13:56:46 -0400

    Update multiplica.cpp

--- Recursao/multiplica.cpp
+++ Recursao/multiplica.cpp
@@ -3,7 +3,7 @@
 int multiplica(int n1, int n2){
     if(n1 == 1){
         return n2;
-    }else if(n1 == 0){
+    }else if(n1 == 0 || n2 == 0){
         return 0;
     }else{
         return n2 + multiplica(n1-1, n2);
