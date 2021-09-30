852541f90d022d8351afc932d3897dc047f304d5
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2021-09-30 13:55:33 -0400

    Create multiplica.cpp

---
+++ Recursao/multiplica.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int multiplica(int n1, int n2){
+    if(n1 == 1){
+        return n2;
+    }else if(n1 == 0){
+        return 0;
+    }else{
+        return n2 + multiplica(n1-1, n2);
+    }
+
+}
+
+int main(){
+
+    printf("%d", multiplica(3, 9));
+
+    return 0;
+}
